a1d0b1f14cc0a354f9c38afab2e3bdb8af2ee60a
Ektoras Simistiras <esimis@eaddon.local>
2021-06-04 15:10:09 +0300

    weird bug on SITE

--- app/pages/Login/vm.js
+++ app/pages/Login/vm.js
@@ -8,36 +8,46 @@ class VM{
         //=====================================================================
         this.username=ko.observable();
         this.password=ko.observable();
+        this.emp_ID=ko.observable();
 
         this.ID=ko.observable();
         this.endPoint = "https://zeus.eaddon.local:9999/Thesis/GetProgram/1/2021/5";
 
     }
 
-   async _loginRequest(){
-       try{
-           let options={
-               method: "GET",
-               credentials:"include",
-               headers:{
-                   Authorization:"Basic "+ btoa(this.username+":"+this.password)
-               }
-           }
 
-           let response= await fetch("https://zeus.eaddon.local:9999/Thesis/GetProgram/1/2021/5",options);
-
-           if (response.ok){
-               this.ID="accepted"; // response.custom me8odos
-           }
-
-           else {throw Error(response.statusText);}
-
-       }
-       catch(e){
-           alert(e.message);
-       }
-
+    async _login(un,ps) { 
+        let request = "https://zeus.eaddon.local:9999/Thesis/Login"
+        try {
+            let options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },                
+                body: JSON.stringify({un,ps})
+            }
+            let response = await fetch(request, options);
+            if (response.ok) {
+                let serviceData = await response.json();
+                return serviceData;
+            }
+            else
+                throw Error(response.statusText);
+        } catch (e) {
+            alert(e.message);
+        }
     }
+
+async _onLogin(){
+
+    this.emp_ID= await this._login(this.username(),this.password());
+    console.log(this.emp_ID);
+    if (this.emp_ID!=null){
+       this.navigateDashboard();
+    }
+    
+}
+
 
 navigateDashboard(){
 
